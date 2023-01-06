83e6974eed30feb863d3b9cd9fea75beb34cd892
Bacancy <Bacancy@DESKTOP-AMR2RQD>
2023-01-06 17:26:47 +0530

    Commit from branchA

--- src/test/java/com/tunescriber/testCases/TC_HomeTest_VerifyFooterLinks_002.java
+++ src/test/java/com/tunescriber/testCases/TC_HomeTest_VerifyFooterLinks_002.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import com.tunescriber.pageObjects.HomePage;
 
-public class TC_HomeTest_001 extends BaseClass {
+public class TC_HomeTest_VerifyFooterLinks_002 extends BaseClass {
 
 	@Test
 	public void verifyLogoLink() {
